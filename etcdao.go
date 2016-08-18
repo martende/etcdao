a399ff9a953835081d6fd1205d922b857e0df057
Oleg <entcrd@gmail.com>
2016-08-18 09:25:38 +0200

    Tests fix

--- etcdao.go
+++ etcdao.go
@@ -1,24 +1,26 @@
 package etcdao
 
-import 
-	"golang.org/x/net/context"
+import (
+	"errors"
 	"github.com/coreos/etcd/client"
+	"golang.org/x/net/context"
+	"log"
 	"reflect"
 	"strconv"
-	"errors"
 	"strings"
 	"time"
 )
 
 var ErrBadFormat = errors.New("ErrBadFormat")
+var DEBUG = false
 
-func processNode(node *client.Node,v interface {},tag reflect.StructTag) error{
+func processNode(node *client.Node, v interface{}, tag reflect.StructTag) error {
 	var err error
 	switch v := v.(type) {
 	case *int:
-		*v,err = strconv.Atoi(node.Value)
-		if err != nil {
-			log.Warnf("processNode: node.Value is not int '%v'",node.Value)
+		*v, err = strconv.Atoi(node.Value)
+		if DEBUG && err != nil {
+			log.Printf("processNode: node.Value is not int '%v'", node.Value)
 		}
 	case *string:
 		*v = node.Value
@@ -27,12 +29,12 @@ func processNode(node *client.Node,v interface {},tag reflect.StructTag) error{
 		if format == "" {
 			format = "2006-01-02"
 		}
-		*v , err = time.Parse(format,node.Value)
-		if err != nil {
-			log.WithError(err).Warnf("processNode: node.Value is not time '%v' for format '%s'",node.Value,format)
+		*v, err = time.Parse(format, node.Value)
+		if DEBUG && err != nil {
+			log.Printf("processNode: node.Value is not time '%v' for format '%s' err=%v", node.Value, format, err)
 		}
 	case *bool:
-		if node.Value != "" && node.Value != "0" && node.Value != "false"  {
+		if node.Value != "" && node.Value != "0" && node.Value != "false" {
 			*v = true
 		} else {
 			*v = false
@@ -41,60 +43,59 @@ func processNode(node *client.Node,v interface {},tag reflect.StructTag) error{
 		tp := reflect.TypeOf(v).Elem()
 		switch tp.Kind() {
 		case reflect.Map:
-			if ! node.Dir {
-				log.Warnf("processNode: type %v needs dir in etcd , not scalar",reflect.TypeOf(v))
+			if DEBUG && !node.Dir {
+				log.Printf("processNode: type %v needs dir in etcd , not scalar", reflect.TypeOf(v))
 				return ErrBadFormat
 			}
 
 			mapElem := reflect.MakeMap(tp)
 
 			//maxVal := 0
-			for _,v := range node.Nodes {
-				li := strings.LastIndex(v.Key,"/")
+			for _, v := range node.Nodes {
+				li := strings.LastIndex(v.Key, "/")
 				fieldName := v.Key[li+1:]
 				newValPtr := reflect.New(tp.Elem())
 
-				err = processNode(v,newValPtr.Interface(),tag)
+				err = processNode(v, newValPtr.Interface(), tag)
 				if err != nil {
 					return err
 				}
 
-				mapElem.SetMapIndex(reflect.ValueOf(fieldName),newValPtr.Elem())
+				mapElem.SetMapIndex(reflect.ValueOf(fieldName), newValPtr.Elem())
 
 			}
-
 
 			reflect.ValueOf(v).Convert(reflect.TypeOf(v)).Elem().Set(mapElem)
 
 		case reflect.Slice:
-			if ! node.Dir {
-				log.Warnf("processNode: type %v needs dir in etcd , not scalar",reflect.TypeOf(v))
+			if DEBUG && !node.Dir {
+				log.Printf("processNode: type %v needs dir in etcd , not scalar", reflect.TypeOf(v))
 				return ErrBadFormat
 			}
 
 			dirFields := make(map[int]*client.Node)
 			maxVal := 0
-			for _,v := range node.Nodes {
-				li := strings.LastIndex(v.Key,"/")
-				fieldName,err := strconv.Atoi(v.Key[li+1:])
-				if err != nil {
-					log.Warnf("processNode: dir element '%s' must be integer",v.Key[li+1:])
+			for _, v := range node.Nodes {
+				li := strings.LastIndex(v.Key, "/")
+				fieldName, err := strconv.Atoi(v.Key[li+1:])
+				if DEBUG && err != nil {
+					log.Printf("processNode: dir element '%s' must be integer", v.Key[li+1:])
 					return ErrBadFormat
 				}
 				dirFields[fieldName] = v
-				if maxVal <  fieldName {
+				if maxVal < fieldName {
 					maxVal = fieldName
 				}
 			}
 
-			sliceElem := reflect.MakeSlice(tp,maxVal+1,maxVal+1)
+			sliceElem := reflect.MakeSlice(tp, maxVal+1, maxVal+1)
 
-			for i := 0 ; i <= maxVal ; i++ {
+			for i := 0; i <= maxVal; i++ {
 				if dirFields[i] != nil {
 					curValue := sliceElem.Index(i)
 					newValPtr := reflect.New(curValue.Type())
 
-					err = processNode(dirFields[i],newValPtr.Interface(),tag)
+					err = processNode(dirFields[i], newValPtr.Interface(), tag)
 					if err != nil {
 						return err
 					}
@@ -103,22 +104,21 @@ func processNode(node *client.Node,v interface {},tag reflect.StructTag) error{
 			}
 			reflect.ValueOf(v).Convert(reflect.TypeOf(v)).Elem().Set(sliceElem)
 
-
 		case reflect.Struct:
-			if ! node.Dir {
-				log.Warnf("processNode: type %v needs dir in etcd , not scalar",reflect.TypeOf(v))
+			if DEBUG && !node.Dir {
+				log.Printf("processNode: type %v needs dir in etcd , not scalar", reflect.TypeOf(v))
 				return ErrBadFormat
 			}
 			dirFields := make(map[string]*client.Node)
-			for _,v := range node.Nodes {
-				li := strings.LastIndex(v.Key,"/")
+			for _, v := range node.Nodes {
+				li := strings.LastIndex(v.Key, "/")
 				dirFields[v.Key[li+1:]] = v
 			}
 
 			svals := reflect.ValueOf(v).Convert(reflect.TypeOf(v)).Elem()
 
 			fc := tp.NumField()
-			for i:= 0 ; i < fc ; i++ {
+			for i := 0; i < fc; i++ {
 				fieldTp := tp.Field(i)
 				//fieldElem := svals.Field(i)
 				//log.Println(fieldElem)
@@ -126,20 +126,22 @@ func processNode(node *client.Node,v interface {},tag reflect.StructTag) error{
 				if fieldName == "" {
 					fieldName = fieldTp.Name
 				}
-				if fieldNode,ok := dirFields[fieldName] ; ok {
+				if fieldNode, ok := dirFields[fieldName]; ok {
 					curValue := svals.Field(i)
 					newValPtr := reflect.New(fieldTp.Type)
 
-					err = processNode(fieldNode,newValPtr.Interface(),fieldTp.Tag)
+					err = processNode(fieldNode, newValPtr.Interface(), fieldTp.Tag)
 					if err != nil {
 						return err
 					}
-					curValue.Set(newValPtr.Elem()/*fieldVal.Elem()*/)
+					curValue.Set(newValPtr.Elem() /*fieldVal.Elem()*/)
 					//log.Warnf(" field '%s' type: %v curVal=%v",fieldTp.Name,fieldTp,newValPtr.Elem())
 				}
 			}
 		default:
-			log.Warnf("processNode: Unkonwn type %v KIND=%v",reflect.TypeOf(v),tp.Kind())
+			if DEBUG {
+				log.Printf("processNode: Unkonwn type %v KIND=%v", reflect.TypeOf(v), tp.Kind())
+			}
 			return ErrBadFormat
 		}
 	}
@@ -157,13 +159,13 @@ func processNode(node *client.Node,v interface {},tag reflect.StructTag) error{
 		slices - reads array from etcd directory - awaits 0 , 1 , 2 ... etc as keys
 		map
 
- */
-func ReadObject(kapi,ctxt,path string, v interface {} ) error {
-	resp, err := kapi.Get(ctxt, path, &client.GetOptions{Recursive:true})
+*/
+func ReadObject(kapi client.KeysAPI, ctxt context.Context, path string, v interface{}) error {
+	resp, err := kapi.Get(ctxt, path, &client.GetOptions{Recursive: true})
 	if err != nil {
 		return err
 	}
 
-	return processNode(resp.Node,v,"")
+	return processNode(resp.Node, v, "")
 
 }
--- etcdao_test.go
+++ etcdao_test.go
@@ -1,127 +1,147 @@
 package etcdao
 
-
 import (
-	"testing"
 	"github.com/coreos/etcd/client"
 	"golang.org/x/net/context"
+	"testing"
+	"time"
 )
 
+func getKapi() (client.KeysAPI, error) {
+	cfg := client.Config{
+		Endpoints:               []string{"http://127.0.0.1:2379"},
+		Transport:               client.DefaultTransport,
+		HeaderTimeoutPerRequest: 5 * time.Second,
+	}
 
-func TestReflectionScalars(t*testing.T) {
-	if  testing.Short() {
+	c, err := client.New(cfg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client.NewKeysAPI(c), nil
+}
+
+func TestReflectionScalars(t *testing.T) {
+	if testing.Short() {
 		t.Skip("Integration")
 	}
 
-	etcd, err := New("/test", []string{"http://127.0.0.1:2379"})
+	kapi, err := getKapi()
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	kapi.Set(context.Background(), "/testStr/testInt", "1", nil)
+
+	defer func() {
+		kapi.Delete(context.Background(), "/testStr", &client.DeleteOptions{Recursive: true})
+	}()
+
 	intV := 0
-	err = etcd.ReadObject(kapi,context.Background()"testInt",&intV)
+	err = ReadObject(kapi, context.Background(), "/testStr/testInt", &intV)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if intV != 1 {
-		t.Fatalf("intV != 1 but = %d",intV)
+		t.Fatalf("intV != 1 but = %d", intV)
 	}
 
 	strV := ""
 
-	err = etcd.ReadObject(kapi,context.Background()"testInt",&strV)
+	err = ReadObject(kapi, context.Background(), "/testStr/testInt", &strV)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if strV != "1" {
-		t.Fatalf("intV != 1 but = %s",strV)
+		t.Fatalf("strV != 1 but = %s", strV)
 	}
 
 }
 
-func TestReflectionStruct(t*testing.T) {
-	if  testing.Short() {
+func TestReflectionStruct(t *testing.T) {
+	if testing.Short() {
 		t.Skip("Integration")
 	}
-	etcd, err := New("/test", []string{"http://127.0.0.1:2379"})
+
+	kapi, err := getKapi()
 	if err != nil {
-		t.Error(err)
-		t.Fail()
+		t.Fatal(err)
 	}
 
-	etcd.WriteKey("/testStr/A","1000")
-	etcd.WriteKey("/testStr/B","BBB")
-	etcd.WriteKey("/testStr/C","")
-	etcd.WriteKey("/testStr/D","false")
-	etcd.WriteKey("/testStr/E","1")
+	kapi.Set(context.Background(), "/testStr/A", "bar", nil)
+	kapi.Set(context.Background(), "/testStr/A", "1000", nil)
+	kapi.Set(context.Background(), "/testStr/B", "BBB", nil)
+	kapi.Set(context.Background(), "/testStr/C", "", nil)
+	kapi.Set(context.Background(), "/testStr/D", "false", nil)
+	kapi.Set(context.Background(), "/testStr/E", "1", nil)
 
 	defer func() {
-		kapi,root := etcd.Kapi()
-		kapi.Delete(context.Background(),root + "/testStr",&client.DeleteOptions{Recursive:true})
+		kapi.Delete(context.Background(), "/testStr", &client.DeleteOptions{Recursive: true})
 	}()
 
 	var str1 struct {
-		A int
-		B string
-		C,D,E bool
+		A       int
+		B       string
+		C, D, E bool
 	}
 
-	err = etcd.ReadObject(kapi,context.Background()"testStr",&str1)
+	err = ReadObject(kapi, context.Background(), "/testStr", &str1)
 
 	if str1.A != 1000 {
-		t.Errorf("str1.A !=1000 str1.A = %v",str1.A )
+		t.Errorf("str1.A !=1000 str1.A = %v", str1.A)
 	}
 
 	if str1.B != "BBB" {
-		t.Errorf("str1.A !=1000 str1.A = %v",str1.A )
+		t.Errorf("str1.A !=1000 str1.A = %v", str1.A)
 	}
 
 	if str1.C != false {
-		t.Errorf("str1.C !=false str1.C = %v",str1.C )
+		t.Errorf("str1.C !=false str1.C = %v", str1.C)
 	}
 
 	if str1.D != false {
-		t.Errorf("str1.D !=false str1.D = %v",str1.C )
+		t.Errorf("str1.D !=false str1.D = %v", str1.C)
 	}
 
 	if str1.E != true {
-		t.Errorf("str1.E !=true str1.E = %v",str1.C )
+		t.Errorf("str1.E !=true str1.E = %v", str1.C)
 	}
 
 }
 
-
-func TestReflectionInnerStruct(t*testing.T) {
-	if  testing.Short() {
+func TestReflectionInnerStruct(t *testing.T) {
+	if testing.Short() {
 		t.Skip("Integration")
 	}
-	etcd, err := New("/test", []string{"http://127.0.0.1:2379"})
+
+	kapi, err := getKapi()
 	if err != nil {
-		t.Error(err)
-		t.Fail()
+		t.Fatal(err)
 	}
-	etcd.WriteKey("/testStrIn/AEL","10")
-	etcd.WriteKey("/testStrIn/B/BEL","20")
-	etcd.WriteKey("/testStrIn/B/BEL2","BEL2VAL")
+
+	kapi.Set(context.Background(), "/testStrIn/AEL", "10", nil)
+	kapi.Set(context.Background(), "/testStrIn/B/BEL", "20", nil)
+	kapi.Set(context.Background(), "/testStrIn/B/BEL2", "BEL2VAL", nil)
 
 	defer func() {
-		kapi,root := etcd.Kapi()
-		kapi.Delete(context.Background(),root + "/testStrIn",&client.DeleteOptions{Recursive:true})
+		kapi.Delete(context.Background(), "/testStrIn", &client.DeleteOptions{Recursive: true})
 	}()
 
 	type B struct {
-		BEL int
+		BEL  int
 		BEL2 string
 	}
 
 	type A struct {
 		AEL int
-		B B
+		B   B
 	}
 
 	a := A{}
 
-	err = etcd.ReadObject(kapi,context.Background(),"/test/testStrIn",&a)
+	err = ReadObject(kapi, context.Background(), "/testStrIn", &a)
 
 	if err != nil {
 		t.Error(err)
@@ -129,38 +149,39 @@ func TestReflectionInnerStruct(t*testing.T) {
 	}
 
 	if a.AEL != 10 {
-		t.Errorf("a.AEL !=10 a.AEL = %v",a.AEL )
+		t.Errorf("a.AEL !=10 a.AEL = %v", a.AEL)
 	}
 
 	if a.B.BEL != 20 {
-		t.Errorf("a.B.BEL !=20 a.B.BEL = %v",a.B.BEL )
+		t.Errorf("a.B.BEL !=20 a.B.BEL = %v", a.B.BEL)
 	}
 
 	if a.B.BEL2 != "BEL2VAL" {
-		t.Errorf("a.B.BEL2 !='BEL2VAL' a.B.BEL2 = %v",a.B.BEL2 )
+		t.Errorf("a.B.BEL2 !='BEL2VAL' a.B.BEL2 = %v", a.B.BEL2)
 	}
 
 }
 
-func TestSliceInt(t*testing.T) {
-	if  testing.Short() {
+func TestSliceInt(t *testing.T) {
+	if testing.Short() {
 		t.Skip("Integration")
 	}
-	etcd, err := New("/test", []string{"http://127.0.0.1:2379"})
+
+	kapi, err := getKapi()
 	if err != nil {
 		t.Fatal(err)
 	}
-	etcd.WriteKey("/testArr/0","10")
-	etcd.WriteKey("/testArr/1","20")
-	etcd.WriteKey("/testArr/2","30")
+
+	kapi.Set(context.Background(), "/testArr/0", "10", nil)
+	kapi.Set(context.Background(), "/testArr/1", "20", nil)
+	kapi.Set(context.Background(), "/testArr/2", "30", nil)
 	defer func() {
-		kapi,root := etcd.Kapi()
-		kapi.Delete(context.Background(),root + "/testArr",&client.DeleteOptions{Recursive:true})
+		kapi.Delete(context.Background(), "/testArr", &client.DeleteOptions{Recursive: true})
 	}()
 
 	var a []int
 
-	err = etcd.ReadObject(kapi,context.Background(),"/test/testArr",&a)
+	err = ReadObject(kapi, context.Background(), "/testArr", &a)
 
 	if err != nil {
 		t.Fatal(err)
@@ -170,13 +191,13 @@ func TestSliceInt(t*testing.T) {
 		t.Fatalf("a = nil")
 	}
 	if len(a) != 3 {
-		t.Fatalf("len(a) != 3 a=%v",a)
+		t.Fatalf("len(a) != 3 a=%v", a)
 	}
 
-	for i := 0 ; i < 3 ; i++ {
-		tv := 10 + i * 10
+	for i := 0; i < 3; i++ {
+		tv := 10 + i*10
 		if a[i] != tv {
-			t.Fatalf("a[%d] != %d a[%d]=%d",i,tv,i,a[0])
+			t.Fatalf("a[%d] != %d a[%d]=%d", i, tv, i, a[0])
 		}
 	}
 
